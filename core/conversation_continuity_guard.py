fe2a82f7362124a316cb178c15fc292d4e5785f8
CristhianneDeLeón <cristhianne.deleon@gmail.com>
2026-09-17 21:55:30 -0600

    Protect profile summary responses from final guards

--- core/conversation_continuity_guard.py
+++ core/conversation_continuity_guard.py
@@ -478,6 +478,7 @@ class ConversationContinuityGuard:
             "routine_memory_update",
             "speaker_identity_memory",
             "active_profile_identity",
+            "active_profile_summary",
             "user_context_explicit_recall",
         }
         if any(source in protected_sources for source in sources):
@@ -489,6 +490,7 @@ class ConversationContinuityGuard:
             "speaker_identity_store",
             "speaker_identity_recall",
             "contextual_identity",
+            "profile_summary",
             "explicit_recall",
         }
         return any(
